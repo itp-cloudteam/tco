6b4d85b5ab4ba489db8d9c8053231f027003cedb
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-01-30 12:32:16 -0500

    Create veriables.tf

---
+++ route-tabel/veriables.tf
@@ -0,0 +1,29 @@
+variable "vpc_id"{
+type = string
+}
+
+variable "gateway_id"{
+type = string
+}
+
+
+
+
+ 
+
+variable "region" {
+type = string
+}
+
+variable "region-code"{
+type = string
+}
+
+variable "routetype"{
+type =  string
+default = "public"
+}
+
+variable "env"{
+type = string
+}
