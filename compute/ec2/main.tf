cbe92520426506e514841780cf7d5217d25ad3ce
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-01-30 12:24:22 -0500

    Update main.tf

--- compute/ec2/main.tf
+++ compute/ec2/main.tf
@@ -1,1 +1,8 @@
 
+module "my_ec2" {
+source          = "../module/ec2"
+ec2_count       = 1
+ami_id          = " ami-09d069a04349dc3cb"
+instance_type   = "t2.micro"
+subnet_id       = "${module.my_ec2.subnet_id}"
+}
