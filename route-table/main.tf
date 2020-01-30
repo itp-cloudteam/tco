866821c7fe8bfaae6e65fd4d0b391099c5b41d0f
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-01-30 12:40:06 -0500

    Update main.tf

--- route-table/main.tf
+++ route-table/main.tf
@@ -1,1 +1,15 @@
 
+resource "aws_route_table" "route_table_" {
+  
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = var.gateway_id
+  }
+
+ 
+  tags = {
+    Name = "route"
+  }
+}
