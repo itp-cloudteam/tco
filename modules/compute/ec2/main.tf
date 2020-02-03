543734867c5b4190dc0d9cc455d3807aea67c6f1
gopikrishna72 <46802838+gopikrishna72@users.noreply.github.com>
2020-02-04 00:05:35 +0530

    ec2

--- modules/compute/ec2/main.tf
+++ modules/compute/ec2/main.tf
@@ -1,6 +1,6 @@
 # Create a new instance of the amazon linux
 resource "aws_instance" "web" {
-  count           = "${var.ec2_count}"
+  ec2_count       = "${var.ec2_count}"
   ami_id          = "${var.ami_id}"
   instance_type   = "${var.instance_type}"
   subnet_id       = "${var.subnet_id}"
