5713e7caada2d03bedd5ffd02ea946eb08d1edea
ManikantaReddy035 <manikanta.reddy@itp-inc.com>
2020-02-05 14:34:40 -0500

    root-main

--- root-main.tf
+++ root-main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region     = var.region
-
   access_key = var.access_key
   secret_key = var.secret_key
 
@@ -8,63 +7,54 @@ provider "aws" {
 
 module "vpc" {
 
-  source = "./modules/networking/vpc"
-  
+  source = "github.com/itp-cloudteam/tco//modules//networking//vpc"
   vpc_info = var.vpc_info
   
-  
 }
-
 
 module "eip" {
 
-  source = "./modules/networking/eip"
+  source = "github.com/itp-cloudteam/tco//modules//networking//eip"
   eips = var.eips
+  
 }
-
-
 
 module "subnets" {
 
-  source = "./modules/networking/subnets"
+  source = "github.com/itp-cloudteam/tco//modules//networking//subnets"
   subnets_info = var.subnets_info  
   vpc_id = module.vpc.ids
   
 }
 
-
-
-
-
 module "security_group" {
 
-  source = "./modules/networking/sg"
+  source = "github.com/itp-cloudteam/tco//modules//networking//sg"
   
   sg_meta_data = var.sg_meta_data
   vpc_id = module.vpc.ids
-  
   
 }
 
 module "security_group_rules" {
 
-  source = "./modules/networking/sg_rules"
+  source = "github.com/itp-cloudteam/tco//modules//networking//sg_rules"
   sg_info = module.security_group.sg_info
-  sg_rules = var.sg_rules  
+  sg_rules = var.sg_rules 
+  
 }
 
 module "igw" {
-  source = "./modules/networking/igw"
+
+  source = "github.com/itp-cloudteam/tco//modules//networking//igw"
   igw_info = var.igw_info
   vpc_id = module.vpc.ids
   
-
 }
-
 
 module "nat_gateway" {
 
-  source = "./modules/networking/natgw"
+  source = "github.com/itp-cloudteam/tco//modules//networking//natgw"
   igw = module.igw.ids
   natgw_info = var.natgw_info
   
@@ -76,7 +66,7 @@ module "nat_gateway" {
 
 module "route_tables" {
   
-  source = "./modules/networking/route_table"
+  source = "github.com/itp-cloudteam/tco//modules//networking//route_table"
   route_tables = var.route_tables
   vpc_id = module.vpc.ids
 
@@ -84,12 +74,22 @@ module "route_tables" {
 
 module "nat_gateway_routes" {
 
-  source = "./modules/networking/route/nat_gateway"
+  source = "github.com/itp-cloudteam/tco//modules//networking//route//nat_gateway"
   nat_gateway_routes = var.nat_gateway_routes
   route_table_id = module.route_tables.ids
   nat_gateway_id     = module.nat_gateway.ids
 
 }
+
+module "gateway_routes" {
+
+  source = "github.com/itp-cloudteam/tco//modules//networking//route//gateway"
+  gateway_routes = var.gateway_routes
+  route_table_id = module.route_tables.ids
+  gateway_id     = module.igw.igw_id
+
+}
+
 
 
 
