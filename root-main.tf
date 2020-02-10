0d0b5c8f55bcf168ba8b5a262f7365bdc9ea25e8
vijaykaturi <53830798+vijaykaturi@users.noreply.github.com>
2020-02-10 22:03:44 +0530

    Update root-main.tf

--- root-main.tf
+++ root-main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = var.region
-  access_key = "AKIAUKV5HPCPGPTFYM3H"
-  secret_key = "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"
+  access_key = var.access_key
+  secret_key = var.secret_key
 
 }
 
