d852a3c73e1a277cc4fcdbbff28a0d54ef16524e
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-03-18 12:21:52 -0400

    Update route-table-main.tf

--- modules/networking/route_table/route-table-main.tf
+++ modules/networking/route_table/route-table-main.tf
@@ -8,3 +8,9 @@ resource "aws_route_table" "route_tables" {
     Name = "rtb-${join("",[for i in split("-",each.value[1]): substr(i,0,1)])}-${split("-",each.value[1])[2]}-${split("_",each.key)[0]}-${each.value[2]}" 
   }
 }
+
+#filter = ""
+#vpc_id =""
+#tags = ""
+#name = ""
+#values = ""
