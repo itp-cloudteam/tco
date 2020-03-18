230acdef87495d643bd5bf1032ce2a1fd6c1a2ca
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-03-18 12:50:50 -0400

    Update route-main.tf

--- modules/networking/route/instance/route-main.tf
+++ modules/networking/route/instance/route-main.tf
@@ -8,4 +8,7 @@ resource "aws_route" "instance_id" {
   instance_id                  = var.instance_id[each.value[2]].id
   
 }
-
+#route_table_id = ""
+#destination_cidr_block = ""
+#destination_ipv6_cidr_block = ""
+#instance_id = ""
