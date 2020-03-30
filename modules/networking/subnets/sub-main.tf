7cea01cf5aff5449791c8647eb2438e05e239563
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-03-30 09:54:03 -0400

    Update sub-main.tf

--- modules/networking/subnets/sub-main.tf
+++ modules/networking/subnets/sub-main.tf
@@ -7,8 +7,8 @@ resource "aws_subnet" "subnet" {
   availability_zone = each.value[1]
   #availability_zone_id = "${var.subnet_info["availability_zone_id"]}"
   #ipv6_cidr_block = "${var.subnet_info["ipv6_cidr_block"]}"
-  #map_public_ip_on_lauch = "${var.subnet_info["map_public_ip_on_launch"]}"
   #assign_ipv6_address_on_lauch = "${var.subnet_info["assign_ipv6_address_on_creation"]}"
+  #map_public_ip_on_lauch = "${var.subnet_info["map_public_ip_on_launch"]}"
   tags = {
     Name = "subnet-${join("",[for i in split("-",each.value[1]): substr(i,0,1)])}-${split("-",each.value[1])[2]}-${split("_",each.key)[0]}-${each.value[0]}-${each.value[4]}" 
   }
