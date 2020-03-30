71a6b79692914ba4929ea8934f4fcd77cfd0f814
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-03-30 09:50:37 -0400

    Update main.tf

--- modules/networking/eip/main.tf
+++ modules/networking/eip/main.tf
@@ -7,7 +7,8 @@
  #network_interface = "${var.network_id}"
  #associate_with_pravate_ip = "${var.eip_info["assign_pravate_ip"]}"
  #tags =
+  #public_ipv4_pool = ${var.eip_info["ipv4_cidr_block"]}"
    #Name = "${var.subnet_info["Name"]}"
- #public_ipv4_pool = ${var.eip_info["ipv4_cidr_block"]}"
+ 
  
  }
