0806c392c22b360507ce659f5f1b0b0dc211ffce
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-03-30 09:47:12 -0400

    Update route-main.tf

--- modules/networking/route/egress_only_gateway/route-main.tf
+++ modules/networking/route/egress_only_gateway/route-main.tf
@@ -8,7 +8,7 @@ resource "aws_route" "egress_only_gateway_id" {
   
 }
 
-#route_table_id = ""
+ #route_table_id = ""
 #destination_cidr_block = ""
 #destination_ipv6_cidr_block = ""
 #egress_only_gateway_id = ""
