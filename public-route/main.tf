90ee42184cd8e76413472a8d2d687b93cf332c04
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-04-20 11:19:21 -0400

    Delete main.tf

--- public-route/main.tf
+++
@@ -1,9 +0,0 @@
-
-# Let’s create the route table that we’ll use with our public subnet networking:
-
-resource "aws_route_table" "public-route-table" {
-  vpc_id = "${aws_vpc.production-vpc.id}"
-  tags {
-    Name = "Public-Route-Table"
-  }
-}
