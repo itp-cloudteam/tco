74d60c821a1574d417116eae89025bb06d47f6ba
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-04-30 15:54:40 -0400

    Add files via upload

---
+++ modules/networking/route/network_interface/variables.tf
@@ -0,0 +1,18 @@
+
+
+ 
+
+variable "route_table_id" {
+
+  type = string
+}
+
+variable "network_interface_id" {
+  
+  type = string
+
+}
+
+variable  "interface_route_dest_cidr_block" {
+type=string
+}
