faa6bc5fc765ec3848385b2799c14e058ef3db01
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-04-30 15:45:19 -0400

    Add files via upload

---
+++ modules/networking/route/egress_only_gateway/route-outputs.tf
@@ -0,0 +1,4 @@
+output "ids" {
+
+  value = "${aws_route.egress_only_gateway_id}"
+}
---
+++ modules/networking/route/egress_only_gateway/route-variables.tf
@@ -0,0 +1,19 @@
+
+
+
+
+variable "route_table_id" {
+
+  type = string
+}
+
+variable "egress_only_gateway_id" {
+  
+  type = string
+
+}
+
+
+variable  "egress_route_dest_cidr_block" {
+type=string
+}
