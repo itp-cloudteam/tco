b2c88b7f92dbfaffdb533dd89b00fad2f5944eb9
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-04-30 15:58:02 -0400

    Add files via upload

---
+++ modules/networking/route/vpc_peering/variables.tf
@@ -0,0 +1,19 @@
+
+
+
+
+variable "route_table_id" {
+
+  type = string
+}
+
+variable "vpc_peering_connection_id" {
+  
+  type = string
+
+}
+
+
+variable  "vpc_peering_route_dest_cidr_block" {
+type=string
+}
