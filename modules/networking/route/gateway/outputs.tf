a96b5887f8246685dbd9c378ee54eb13e37e0bb9
farhan0041 <58825269+farhan0041@users.noreply.github.com>
2020-04-30 15:49:32 -0400

    Add files via upload

---
+++ modules/networking/route/gateway/outputs.tf
@@ -0,0 +1,5 @@
+output "ids" {
+
+  value = "${aws_route.gateway_id}"
+
+}
---
+++ modules/networking/route/gateway/variables.tf
@@ -0,0 +1,18 @@
+
+
+ 
+
+variable "route_table_id" {
+
+  type = string
+}
+
+variable "gateway_id" {
+  
+  type = string
+
+}
+
+variable  "gateway_route_dest_cidr_block" {
+type=string
+}
